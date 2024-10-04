6e6be6bb2fd05d1bd41e410eb1de7a0c90b38590
ikenna ughanze <ughanzepolycarp@gmail.com>
2024-10-04 09:47:43 +0100

    update auth.py file, render templates

--- website/auth.py
+++ website/auth.py
@@ -1,16 +1,16 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template,redirect,url_for
 
 auth = Blueprint("auth", __name__)
 
 
 @auth.route("/login")
 def login():
-    return "home"
+    return render_template("log-in.html")
 
 @auth.route("/sign-up")
 def sign_up():
-    return "home"
+    return render_template("sign-up.html")
 
 @auth.route("/log-out")
 def log_out():
-    return ""
+    return redirect(url_for("views.log_in"))
