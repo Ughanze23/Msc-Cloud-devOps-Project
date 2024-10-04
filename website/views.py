2a1d6da3ebd410b52906e6021f9c5ab8e186aec1
ikenna ughanze <ughanzepolycarp@gmail.com>
2024-10-04 08:56:40 +0100

    setup: define functions for all the pages of the website in the views.py file

--- website/views.py
+++ website/views.py
@@ -5,4 +5,24 @@ views = Blueprint("views", __name__)
 @views.route("/home")
 @views.route("/")
 def home():
-    return "home"
+    return render_template("home.html")
+
+@views.route("/log-in")
+def log_in():
+    return render_template("log-in.html")
+
+@views.route("/sign-up")
+def sign_up():
+    return render_template("sign-up.html")
+
+@views.route("/glossary")
+def glossary():
+    return render_template("glossary.html")
+
+@views.route("/post-glossary")
+def post_glossary():
+    return render_template("post-glossary.html")
+
+@views.route("/admin")
+def admin():
+    return render_template("admin.html")
