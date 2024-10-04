9ae754a5ecd086958d99ce1a183579aa8b40ecef
ikenna ughanze <ughanzepolycarp@gmail.com>
2024-10-04 17:18:30 +0100

    add restriction in views.py to only view pages if you are logged in

--- website/views.py
+++ website/views.py
@@ -1,20 +1,25 @@
 from flask import Blueprint, render_template
+from flask_login import login_required,current_user
 
 views = Blueprint("views", __name__)
 
 @views.route("/")
 @views.route("/home")
+@login_required
 def home():
-    return render_template("home.html")
+    return render_template("home.html",name=current_user.username)
 
+@login_required
 @views.route("/glossary")
 def glossary():
     return render_template("glossary.html")
 
+@login_required
 @views.route("/post-glossary")
 def post_glossary():
     return render_template("post-glossary.html")
 
+@login_required
 @views.route("/admin")
 def admin():
-    return render_template("admin.html")
+    return render_template("admin.html",)
