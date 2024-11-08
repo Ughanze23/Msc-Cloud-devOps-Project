6c027f359fa6dfde2bb11989aff9a0594d581983
ikenna ughanze <ughanzepolycarp@gmail.com>
2024-11-08 12:25:43 +0000

    setup: complete CI/CD pipeline

---
+++ terraform/main.tf
@@ -0,0 +1,47 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+  
+  backend "s3" {
+    bucket = "mycppproject23384069"
+    key    = "flask-app/terraform.tfstate"
+    region = "eu-west-1"
+  }
+}
+
+provider "aws" {
+  region = "eu-west-1"
+}
+
+resource "aws_elastic_beanstalk_application" "flask_app" {
+  name        = "loan-business-glossary-app"
+  description = "Loan Business Glossary Flask application deployed through CI/CD"
+}
+
+resource "aws_elastic_beanstalk_environment" "flask_app_env" {
+  name                = "cds-flask-app-env"
+  application         = aws_elastic_beanstalk_application.flask_app.name
+  solution_stack_name = "64bit Amazon Linux 2 v3.3.13 running Python 3.8"
+
+  setting {
+    namespace = "aws:autoscaling:launchconfiguration"
+    name      = "InstanceType"
+    value     = "t2.micro"
+  }
+  
+  setting {
+    namespace = "aws:autoscaling:asg"
+    name      = "MinSize"
+    value     = "1"
+  }
+  
+  setting {
+    namespace = "aws:autoscaling:asg"
+    name      = "MaxSize"
+    value     = "2"
+  }
+}
