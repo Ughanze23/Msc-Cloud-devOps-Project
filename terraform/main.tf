bda1bda273e3688dfc2b45c8d2cba7f20fb80e74
ikenna ughanze <ughanzepolycarp@gmail.com>
2024-11-12 20:53:55 +0000

    add role and instance profile to eb setup on terraform using settings

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,21 +3,13 @@ resource "aws_elastic_beanstalk_environment" "flask_app_env" {
   application         = aws_elastic_beanstalk_application.flask_app.name
   solution_stack_name = "64bit Amazon Linux 2023 v4.3.0 running Python 3.9"
 
-  # Set the instance profile
-  setting {
+  # Add the instance profile reference
+setting {
     namespace = "aws:autoscaling:launchconfiguration"
     name      = "IamInstanceProfile"
-    value     = "LabInstanceProfile"  
+    value     = "arn:aws:iam::205576784570:instance-profile/LabInstanceProfile"
   }
 
-  # Set the service role
-  setting {
-    namespace  = "aws:elasticbeanstalk:environment"
-    name       = "ServiceRole"
-    value      = "LabRole"  
-  }
-
-  # Additional settings for environment capacity
   setting {
     namespace = "aws:autoscaling:launchconfiguration"
     name      = "InstanceType"
