29da4121dd850af8c46c2e81d52834c42a5c6a62
ikenna ughanze <ughanzepolycarp@gmail.com>
2024-11-12 20:24:27 +0000

    update solution stack in main.tf to 64bit Amazon Linux 2023 v4.3.0 running Python 3.9

--- terraform/main.tf
+++ terraform/main.tf
@@ -25,7 +25,7 @@ resource "aws_elastic_beanstalk_application" "flask_app" {
 resource "aws_elastic_beanstalk_environment" "flask_app_env" {
   name                = "cds-flask-app-env"
   application         = aws_elastic_beanstalk_application.flask_app.name
-  solution_stack_name = "64bit Amazon Linux 2023 v4.0.8 running Python 3.9"
+  solution_stack_name = "64bit Amazon Linux 2023 v4.3.0 running Python 3.9"
 
   setting {
     namespace = "aws:autoscaling:launchconfiguration"
