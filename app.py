5ec00f24f654a08495c1a82b21179f34ab2e74b8
ikenna ughanze <ughanzepolycarp@gmail.com>
2024-11-12 20:05:35 +0000

    update CI_CD pipeline, add eb config files to project

--- app.py
+++ app.py
@@ -1,5 +1,10 @@
 from website import create_app
 
+application = create_app()
+
 if __name__ == "__main__":
-    app = create_app()
-    app.run(debug=True)
+    application.run(host='0.0.0.0', port=5000)
+
+#if __name__ == "__main__":
+ #   app = create_app()
+ #   app.run(debug=True)
--- terraform/main.tf
+++ terraform/main.tf
@@ -7,14 +7,14 @@ terraform {
   }
   
   backend "s3" {
-    bucket = "cpp-23384069"
+    bucket = "x23384069-cpp"
     key    = "flask-app/terraform.tfstate"
-    region = "eu-west-1"
+    region = "us-east-1"
   }
 }
 
 provider "aws" {
-  region = "eu-west-1"
+  region = "us-east-1"
 }
 
 resource "aws_elastic_beanstalk_application" "flask_app" {
@@ -25,7 +25,7 @@ resource "aws_elastic_beanstalk_application" "flask_app" {
 resource "aws_elastic_beanstalk_environment" "flask_app_env" {
   name                = "cds-flask-app-env"
   application         = aws_elastic_beanstalk_application.flask_app.name
-  solution_stack_name = "64bit Amazon Linux 2 v3.3.13 running Python 3.8"
+  solution_stack_name = "64bit Amazon Linux 2 v3.3.13 running Python 3.12"
 
   setting {
     namespace = "aws:autoscaling:launchconfiguration"
