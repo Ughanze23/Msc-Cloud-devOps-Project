e5dc00d7ae55e6dcd745ceb674ad2b8bc23deb32
ikenna ughanze <ughanzepolycarp@gmail.com>
2024-11-13 06:36:18 +0000

    update CI/CD config, only create beanstalk application if it doesnot exist

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,21 @@
+# Configure Terraform backend using S3
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+  
+  backend "s3" {
+    bucket = "x23384069-cpp"
+    key    = "flask-app/terraform.tfstate"
+    region = "us-east-1" 
+  }
+}
+
+# Existing resources for your Elastic Beanstalk application
+
 resource "aws_elastic_beanstalk_application" "flask_app" {
   name        = "loan-business-glossary-app"
   description = "Loan Business Glossary Flask application deployed through CI/CD"
@@ -9,7 +27,7 @@ resource "aws_elastic_beanstalk_environment" "flask_app_env" {
   solution_stack_name = "64bit Amazon Linux 2023 v4.3.0 running Python 3.9"
 
   # Add the instance profile reference
-setting {
+  setting {
     namespace = "aws:autoscaling:launchconfiguration"
     name      = "IamInstanceProfile"
     value     = "arn:aws:iam::205576784570:instance-profile/LabInstanceProfile"
