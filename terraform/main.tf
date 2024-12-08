72d9063ec5ed29139895c0cbf9ffdd1e740d639b
ikenna ughanze <ughanzepolycarp@gmail.com>
2024-12-08 13:53:39 +0000

    add eb launch template setting to main.tf file

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,26 +1,14 @@
-# Configure Terraform backend using S3
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.0"
-    }
-  }
-}
-
-# Elastic Beanstalk Application
-resource "aws_elastic_beanstalk_application" "flask_app" {
-  name        = "loan-business-glossary-app"
-  description = "Loan Business Glossary Flask application deployed through CI/CD"
-}
-
 # Elastic Beanstalk Environment
 resource "aws_elastic_beanstalk_environment" "flask_app_env" {
   name                = "cds-flask-app-env-3"
   application         = aws_elastic_beanstalk_application.flask_app.name
-  solution_stack_name = "64bit Amazon Linux 2023 v4.3.1 running Python 3.9" 
+  solution_stack_name = "64bit Amazon Linux 2023 v4.3.1 running Python 3.9"
 
-
+  setting {
+    namespace = "aws:autoscaling:launchconfiguration"
+    name      = "LaunchTemplate"
+    value     = "true"
+  }
 
   # Instance Profile
   setting {
@@ -29,13 +17,13 @@ resource "aws_elastic_beanstalk_environment" "flask_app_env" {
     value     = "arn:aws:iam::339712727128:instance-profile/MyEC2Role"
   }
 
-  # Instance Type
+  # Launch Template
   setting {
-    namespace = "aws:ec2:instances"
-    name      = "InstanceTypes"
+    namespace = "aws:ec2:launchtemplate"
+    name      = "InstanceType"
     value     = "t2.micro"
   }
-  
+
   # Auto Scaling Group Settings
   setting {
     namespace = "aws:autoscaling:asg"
