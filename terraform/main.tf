3c7401ff8f71608ff761bb01074dc27c7f164cf1
ikenna ughanze <ughanzepolycarp@gmail.com>
2024-12-08 15:04:08 +0000

    use launchconfig for eb setup in main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,57 +4,36 @@ resource "aws_elastic_beanstalk_environment" "flask_app_env" {
   application         = aws_elastic_beanstalk_application.flask_app.name
   solution_stack_name = "64bit Amazon Linux 2023 v4.3.1 running Python 3.9"
 
-  setting {
-    namespace = "aws:autoscaling:launchconfiguration"
-    name      = "LaunchTemplate"
-    value     = "true"
-  }
+ # setting {
+ #   namespace = "aws:autoscaling:launchconfiguration"
+  #  name      = "LaunchTemplate"
+  #  value     = "true"
+  #}
 
   # Instance Profile
   setting {
     namespace = "aws:autoscaling:launchconfiguration"
     name      = "IamInstanceProfile"
-    value     = "arn:aws:iam::339712727128:instance-profile/MyEC2Role"
+    value     = "arn:aws:iam::205576784570:instance-profile/LabInstanceProfile"
   }
 
-  # Launch Template
   setting {
-    namespace = "aws:ec2:launchtemplate"
+    namespace = "aws:autoscaling:launchconfiguration"
     name      = "InstanceType"
     value     = "t2.micro"
   }
 
-  # Auto Scaling Group Settings
   setting {
     namespace = "aws:autoscaling:asg"
     name      = "MinSize"
     value     = "1"
   }
-  
+
   setting {
     namespace = "aws:autoscaling:asg"
     name      = "MaxSize"
     value     = "2"
   }
 
-  # Environment Type
-  setting {
-    namespace = "aws:elasticbeanstalk:environment"
-    name      = "EnvironmentType"
-    value     = "LoadBalanced"
-  }
-
-  # Load Balancer Type
-  setting {
-    namespace = "aws:elasticbeanstalk:environment"
-    name      = "LoadBalancerType"
-    value     = "application"
-  }
-
-  # Rolling Updates
-  setting {
-    namespace = "aws:autoscaling:updatepolicy:rollingupdate"
-    name      = "RollingUpdateEnabled"
-    value     = "true"
-  }
+ 
 }
