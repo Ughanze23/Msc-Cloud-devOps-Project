ada7f2f81949108bc63c4b045248608397062684
ikenna ughanze <ughanzepolycarp@gmail.com>
2024-12-08 13:06:19 +0000

    update terraform file to use launch templates instead of launch configs

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,34 +6,42 @@ terraform {
       version = "~> 4.0"
     }
   }
-  
 }
 
-# Existing resources for your Elastic Beanstalk application
-
+# Elastic Beanstalk Application
 resource "aws_elastic_beanstalk_application" "flask_app" {
   name        = "loan-business-glossary-app"
   description = "Loan Business Glossary Flask application deployed through CI/CD"
 }
 
+# Elastic Beanstalk Environment
 resource "aws_elastic_beanstalk_environment" "flask_app_env" {
   name                = "cds-flask-app-env-2"
   application         = aws_elastic_beanstalk_application.flask_app.name
-  solution_stack_name = "64bit Amazon Linux 2023 v4.3.1 running Python 3.9"
+  solution_stack_name = "64bit Amazon Linux  v4.3.1 running Python 3.9" 
 
-  # Add the instance profile reference
+#launch template
+  setting {
+    namespace = "aws:autoscaling:launchconfiguration"
+    name      = "DisableLaunchConfiguration"
+    value     = "true"
+  }
+
+  # Instance Profile
   setting {
     namespace = "aws:autoscaling:launchconfiguration"
     name      = "IamInstanceProfile"
     value     = "arn:aws:iam::339712727128:instance-profile/MyEC2Role"
   }
 
+  # Instance Type
   setting {
-    namespace = "aws:autoscaling:launchconfiguration"
-    name      = "InstanceType"
+    namespace = "aws:ec2:instances"
+    name      = "InstanceTypes"
     value     = "t2.micro"
   }
   
+  # Auto Scaling Group Settings
   setting {
     namespace = "aws:autoscaling:asg"
     name      = "MinSize"
@@ -44,5 +52,26 @@ resource "aws_elastic_beanstalk_environment" "flask_app_env" {
     namespace = "aws:autoscaling:asg"
     name      = "MaxSize"
     value     = "2"
+  }
+
+  # Environment Type
+  setting {
+    namespace = "aws:elasticbeanstalk:environment"
+    name      = "EnvironmentType"
+    value     = "LoadBalanced"
+  }
+
+  # Load Balancer Type
+  setting {
+    namespace = "aws:elasticbeanstalk:environment"
+    name      = "LoadBalancerType"
+    value     = "application"
+  }
+
+  # Rolling Updates
+  setting {
+    namespace = "aws:autoscaling:updatepolicy:rollingupdate"
+    name      = "RollingUpdateEnabled"
+    value     = "true"
   }
 }
