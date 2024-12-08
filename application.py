cfbcf72a120d32d3818c2f0944aae785906f9eac
ikenna ughanze <ughanzepolycarp@gmail.com>
2024-12-08 12:38:13 +0000

    update application.py with configuration ready for production

--- application.py
+++ application.py
@@ -3,6 +3,6 @@ from website import create_app
 application = create_app()
 
 if __name__ == "__main__":
-    #application.run(host='0.0.0.0', port=5000)
-    application.run(debug=True)
+    application.run(host='0.0.0.0', port=5000)
+    #application.run(debug=True)
 
